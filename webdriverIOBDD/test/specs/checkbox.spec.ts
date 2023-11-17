fff94d5b13693266d2fe29e353c93bb87672e97b
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-11-17 18:39:41 +0530

    Update checkbox.spec.ts

--- webdriverIOBDD/test/specs/checkbox.spec.ts
+++ webdriverIOBDD/test/specs/checkbox.spec.ts
@@ -1,13 +1,13 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 
 describe('checkboxes', () =>  {
-    it('checkbox 2 should be enabled', async () =>  {
+    it('TestCaseKey=KAN-T1 testcheckbox 2 should be enabled', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
-    it('checkbox 1 should be enabled after clicking on it', async () =>  {
+    it('TestCaseKey=KAN-T1 checkbox 1 should be enabled after clicking on it', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
