2c13b735342b1252b979f3df84469cc2f7132332
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-11-20 15:14:25 +0530

    Update wdio.shared.conf.ts

--- wdio.shared.conf.ts
+++ wdio.shared.conf.ts
@@ -38,8 +38,7 @@ export const config: WebdriverIO.Config = {
     // will be called from there.
     //
     specs: [
-        //'./test/specs/**/*.ts'
-         './test/specs/**/checkbox.spec.ts'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -135,10 +134,10 @@ export const config: WebdriverIO.Config = {
           disableWebdriverStepsReporting: true,
           disableWebdriverScreenshotsReporting: true,
       }],
-      //
-      // ['json', {
-      //   outputDir: './test/reports/json-results'
-      //   }],
+      
+      ['json', {
+        outputDir: './test/reports/json-results'
+        }],
 
       ['junit', {
         outputDir: './test/reports/junit-results',
