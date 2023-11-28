1eb01eeaebbcbe8e42e146d9bf8cb80cafdca394
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-11-28 17:28:00 +0530

    Update checkbox.spec.ts

--- test/specs/checkbox.spec.ts
+++ test/specs/checkbox.spec.ts
@@ -1,13 +1,13 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 
 describe('checkboxes', () =>  {
-    it('user login with test name', async () =>  {
+    it('user login with test name and pwd', async () =>  {
         //await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
-    it('checkbox 1 should be enabled after clicking on it(Zephyr ID:KAN-T1)', async () =>  {
+    it('checkbox 1 should be enabled after clicking', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
