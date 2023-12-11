3f776d72895f67648eb7580e8f0052154b76aa5c
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-12-11 17:27:19 +0530

    Update yahoo-search-test.spec.ts

--- test/specs/yahoo-search-test.spec.ts
+++ test/specs/yahoo-search-test.spec.ts
@@ -6,13 +6,13 @@ import assert       from 'assert';
 	This is a BDD test using Mocha JavaScript framework
 */
 
-describe('Performing a search operation on Yahoo Page',  () =>  {
-  it('Performing a search operation', async () =>  {
+describe('Performing a search operation on Yahoo webPage',  () =>  {
+  it('Performing a search operation on web', async () =>  {
     await yahooPage.open();
     assert.equal(await browser.getTitle(), 'Yahoo Search - Web Search');
   });
 
-  it('searching Selenium Webdriver', async () =>  {
+  it('searching Selenium Webdriver on web', async () =>  {
     await yahooPage.enterText('Selenium Webdriver');
     yahooPage.search();
     assert.equal(await yahooPage.isSearched(), true);
