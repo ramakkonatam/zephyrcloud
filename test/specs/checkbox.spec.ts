8dabfd0ce0ece44390fcf02e94a4534bb82b2181
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-12-11 17:25:50 +0530

    Update checkbox.spec.ts

--- test/specs/checkbox.spec.ts
+++ test/specs/checkbox.spec.ts
@@ -1,13 +1,13 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 
-describe('Launch the web page and register', () =>  {
-    it('navigate to yahoo page', async () =>  {
+describe('Launch the website page and register', () =>  {
+    it('navigate to yahoo website page', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
-    it('click checkbox', async () =>  {
+    it('click checkbox on website', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
