b53b74ea5b76ea344ea1a8a3eb6bfbf18067f6d5
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-12-12 14:45:06 +0530

    Update checkbox.spec.ts

--- test/specs/checkbox.spec.ts
+++ test/specs/checkbox.spec.ts
@@ -1,14 +1,18 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 import allureReporter from '@wdio/allure-reporter';
 describe('Launch the website page and register', () =>  {
-    it('User authentication with email and password', async () =>  {
-        allureReporter.addAllureId("366")
+    it('Create user and give access', async () =>  {
+        allureReporter.addAllureId("367")
+        allureReporter.addStep("Navigateto the website")
         await CheckboxPage.open()
+        allureReporter.addStep("Create new user")
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
     it('click checkbox on website', async () =>  {
+        allureReporter.addAllureId("367")
+        allureReporter.addStep("Grant new access")
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
