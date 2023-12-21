4843ba90ce784c206da07a4fafbee100767e66be
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-12-21 13:18:24 +0530

    Update checkbox.spec.ts

--- test/specs/checkbox.spec.ts
+++ test/specs/checkbox.spec.ts
@@ -1,17 +1,17 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 import allureReporter from '@wdio/allure-reporter';
 describe('Launch the website page and register', () =>  {
-    it('Authentication with user details', async () =>  {
-        allureReporter.addTag("web")
-        allureReporter.addAllureId("398")
-        allureReporter.addStep("Clickon signup button")
+    it('As a user, I can log into the secure area', async () =>  {
+        // allureReporter.addTag("web")
+        // allureReporter.addAllureId("398")
+        // allureReporter.addStep("Clickon signup button")
         await CheckboxPage.open()
         allureReporter.addStep("Create new user")
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
-    it('Navigate to signup page', async () =>  {
+    it('Sample Feature: Sample Scenario', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
