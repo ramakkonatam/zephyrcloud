553d0431a62c4bc2269f7292457b2124810126ec
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-12-26 14:29:21 +0530

    Update checkbox.spec.ts

--- test/specs/checkbox.spec.ts
+++ test/specs/checkbox.spec.ts
@@ -1,22 +1,19 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 import allureReporter from '@wdio/allure-reporter';
 describe('Launch the website page and register', () =>  {
-    it('Launch the website and complete the user registration', async () =>  {
-        // allureReporter.addTag("web")
-        allureReporter.addAllureId("563")
-        // allureReporter.addStep("Clickon signup button")
-        // allureReporter.addTestId("BAC-T17")
-        // allureReporter.addLabel("zephyr","BAC-T17")
+    it('As a user, I can log into the secure website', async () =>  {
+        allureReporter.addTag("web")
+        // allureReporter.addAllureId("563")
+        //allureReporter.addTestId("BAC-T17")
+        allureReporter.addLabel("zephyr","BAC-T17")
         await CheckboxPage.open()
-        allureReporter.addStep("Launch the website https://yahoo.com")
-        allureReporter.addStep("Clickon the user registration")
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
-    it.skip('Sample Feature: Sample Scenario web', async () =>  {
+    it('Sample Feature: Sample Scenario web', async () =>  {
         // allureReporter.addTestId("BAC-T18")
-        // allureReporter.addLabel("zephyr","BAC-T18")
+        allureReporter.addLabel("zephyr","BAC-T18")
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
